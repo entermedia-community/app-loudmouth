ea0413d9fbd5c266afcdc90a7523bda6f0aca19d
cburkey <cburkey@entermediasoftware.com>
2011-07-18 09:42:02 -0400

    Michael Tweak
    Change-Id: I6a4bca952fb31df0e920e29472bd9df93bf3722d

--- etc/install.js
+++ etc/install.js
@@ -4,7 +4,7 @@ importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
 
-var war = "http://dev.entermediasoftware.com/projects/loudmouth/ROOT.war";
+var war = "http://dev.entermediasoftware.com/jenkins/job/app-loudmouth/lastSuccessfulBuild/artifact/deploy/ROOT.war";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
